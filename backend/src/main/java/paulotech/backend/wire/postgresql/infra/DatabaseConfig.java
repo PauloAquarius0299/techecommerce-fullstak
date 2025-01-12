ba187b1f90275e51457206a13faf4cbdcbc6f903
Paulo Cesar <paulocgsantos99@gmail.com>
2025-01-12 15:27:32 -0300

    feat: add cors config, bean validation errors and db config

---
+++ backend/src/main/java/paulotech/backend/wire/postgresql/infra/DatabaseConfig.java
@@ -0,0 +1,15 @@
+package paulotech.backend.wire.postgresql.infra;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.data.web.config.EnableSpringDataWebSupport;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+@Configuration
+@EnableTransactionManagement
+@EnableJpaRepositories(basePackages = {"fr.paulotech.app"})
+@EnableJpaAuditing
+@EnableSpringDataWebSupport(pageSerializationMode = EnableSpringDataWebSupport.PageSerializationMode.VIA_DTO)
+public class DatabaseConfig {
+}
---
+++ backend/src/main/java/paulotech/backend/wire/security/primary/CorsFilterConfiguration.java
@@ -0,0 +1,24 @@
+package paulotech.backend.wire.security.primary;
+
+import lombok.RequiredArgsConstructor;
+import org.apache.catalina.filters.CorsFilter;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.Ordered;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+@Configuration
+@RequiredArgsConstructor
+public class CorsFilterConfiguration {
+
+    private CorsConfiguration  corsConfiguration;
+
+    public FilterRegistrationBean<CorsFilter> simpleCorsFilter(){
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", corsConfiguration);
+        FilterRegistrationBean<CorsFilter> bean = new FilterRegistrationBean<>(new CorsFilter());
+        bean.setOrder(Ordered.HIGHEST_PRECEDENCE);
+        return bean;
+    }
+}
