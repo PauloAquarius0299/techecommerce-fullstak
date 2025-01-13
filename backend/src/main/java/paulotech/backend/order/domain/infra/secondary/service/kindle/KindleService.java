a7628cef84ed6325e3f9ed955005eda4322d4c99
Paulo Cesar <paulocgsantos99@gmail.com>
2025-01-13 11:10:33 -0300

    feat: add service token access

--- backend/src/main/java/paulotech/backend/order/domain/infra/secondary/service/kindle/KindleService.java
+++ backend/src/main/java/paulotech/backend/order/domain/infra/secondary/service/kindle/KindleService.java
@@ -1,9 +1,19 @@
 package paulotech.backend.order.domain.infra.secondary.service.kindle;
 
+import org.apache.hc.core5.http.ContentType;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.ParameterizedTypeReference;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestClient;
+
+import java.net.URLEncoder;
+import java.nio.charset.StandardCharsets;
+import java.util.Base64;
+import java.util.Map;
+import java.util.Optional;
 
 @Service
 public class KindleService {
@@ -24,4 +34,37 @@ public class KindleService {
             .requestFactory(new HttpComponentsClientHttpRequestFactory())
             .baseUrl(kindleApi)
             .build();
+
+    private Optional<String> getToken(){
+        try{
+            ResponseEntity<KindleAccessToken> accessToken = restClient.post()
+                    .uri(kindleApi + "/oauth/token")
+                    .body("grant_type=client_credentials&audience=" + URLEncoder.encode(audience, StandardCharsets.UTF_8))
+                    .accept(MediaType.APPLICATION_JSON)
+                    .contentType(MediaType.APPLICATION_FORM_URLENCODED)
+                    .header("Authorization",
+                            "Basic " + Base64.getEncoder().encodeToString((clientId + ":" + clientSecret).getBytes(StandardCharsets.UTF_8)))
+                .header("Content-Type", ContentType.APPLICATION_FORM_URLENCODED.getMimeType())
+                    .retrieve()
+                    .toEntity(KindleAccessToken.class);
+            return Optional.of(accessToken.getBody().accessToken());
+        } catch (Exception e){
+            return Optional.empty();
+        }
+    }
+
+    public Map<String,Object> getUserInfo( String userId ){
+        String token = getToken().orElseThrow(() -> new IllegalStateException("Nenhum token encontrado"));
+
+        var typeRef = new ParameterizedTypeReference<Map<String, Object>>() {};
+
+        ResponseEntity<Map<String, Object>> authorization = restClient.get()
+                .uri(kindleApi + "/api/v1/user?id={id}" + userId)
+                .accept(MediaType.APPLICATION_JSON)
+                .header("Authorization", "Bearer " + token)
+                .retrieve()
+                .toEntity(typeRef);
+
+        return authorization.getBody();
+    }
 }
